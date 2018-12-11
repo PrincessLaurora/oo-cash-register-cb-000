e83097d4e2290e26465a8ad3c0abe335271c311a
Laura Felce <crepu83@yahoo.it>
2018-12-11 09:33:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+def initialize(discount = 0)
+@total = 0
+@discount = discount
+end
+
+def add_item(title, price, quantity = 1)
+  self.total =+ price * quantity
+end
+
+end
